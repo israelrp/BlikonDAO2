e07a512a6ea4265ba8660a8d2c8714cdf76eda3f
Alejandro Arrieta <arrieta810@gmail.com>
2023-09-12 16:26:18 -0600

    Método para generar consultas dinámicas

--- DBMongoDDL/Administrador.cs
+++ DBMongoDDL/Administrador.cs
@@ -5,6 +5,7 @@ using MongoDB.Driver;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using BlikonDAO.Models;
 
 namespace DBMongoDDL
 {
@@ -101,6 +102,67 @@ namespace DBMongoDDL
             {
                 oRespuesta.Mensaje = "Ocurrió un error al procesar su solicitud: " + e.Message;
             }
+            return oRespuesta;
+        }
+
+        public async Task<Response> QueryGetAllAppItems(QueryOperators model)
+        {
+            Response oRespuesta = new();
+            try
+            {
+                var database = _client.GetDatabase("prueba");
+                var collection = database.GetCollection<Item>("Items");
+
+                var builder = Builders<Item>.Filter;
+                List<FilterDefinition<Item>> lstfilter = new();
+                FilterDefinition<Item> filter = null;
+                foreach (var item in model.lstFieldsQuery)
+                {
+                    if (item.operador == "Eq")
+                    {
+                        lstfilter.Add(builder.Eq(item.fieldName, item.fieldValue));
+                    }
+                    else if (item.operador == "And")
+                    {
+                        lstfilter.Add(builder.And(item.fieldName, item.fieldValue));
+                    }
+                    else if (item.operador == "Or")
+                    {
+                        lstfilter.Add(builder.Or(item.fieldName, item.fieldValue));
+                    }
+                    else if (item.operador == "In")
+                    {
+                        lstfilter.Add(builder.In(item.fieldName, item.fieldValue));
+                    }
+                    else if (item.operador == "lt")
+                    {
+                        lstfilter.Add(builder.Lt(item.fieldName, item.fieldValue));
+                    }
+                }
+
+                if (model.LogicOperator == "And")
+                {
+                    filter = builder.And(lstfilter);
+                }
+                else if (model.LogicOperator == "Or")
+                {
+                    filter = builder.Or(lstfilter);
+                }
+                else if (model.LogicOperator == "")
+                {
+                    filter = lstfilter.FirstOrDefault();
+                }
+
+
+                var result = await collection.FindAsync(filter);
+                oRespuesta.Data = result;
+                oRespuesta.Exito = 1;
+            }
+            catch(Exception e)
+            {
+                oRespuesta.Mensaje = "Ocurrió un error al procesar su solicitud: " + e.Message;
+            }
+            
             return oRespuesta;
         }
 
---
+++ DBMongoDDL/Models/QueryOperators.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlikonDAO.Models
+{
+    public class QueryOperators
+    {
+        public List<FieldQuery> lstFieldsQuery { get; set; }
+        public string LogicOperator { get; set; }
+    }
+}
